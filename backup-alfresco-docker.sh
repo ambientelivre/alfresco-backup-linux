5a19a00fba32688d83fac0f0f139f3fd7e5af8ed
Marcio Junior Vieira <marcio@ambientelivre.com.br>
2025-09-27 10:32:36 -0300

    Update backup-alfresco-docker.sh

--- backup-alfresco-docker.sh
+++ backup-alfresco-docker.sh
@@ -7,7 +7,7 @@
 # Create by marcio@ambientelivre.com.br marcos@ambientelivre.com.br
 
 # Configs of Script
-DESTDIR="/home/$(whoami)/backup"      	             # backup destination directory
+DESTDIR="/opt/backup"      	                         # backup destination directory
 DATE_NOW=$(date +%d-%m-%y)                           # default filename with date
 INSTALL_ALFRESCO=/opt/alfresco                	     # directory where alfresco is installed 
 DIR_ALFDATA="${INSTALL_ALFRESCO}/data/alf-repo-data" # alfresco data directory (alf-data)
